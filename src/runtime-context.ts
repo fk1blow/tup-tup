017a47643ad278493e6feae66aad7018339dc861
Dragos Tudorache <hedphug@gmail.com>
2026-03-15 21:01:55 +0200

    Add todo

--- src/runtime-context.ts
+++ src/runtime-context.ts
@@ -7,5 +7,25 @@ export interface RuntimeContext {
   workspacePath: string
   artifactsPath: string
   logsPath: string
+  // What's the appPath for? What app?
+  // TODO let's document this better
+  // const runtimeCtx = {
+  //   repoUrl: 'https://github.com/fk1blow/tup-tup-demo-repo',
+  //   repoBranch: undefined,
+  //   workspacePath:
+  //     '/Users/dragostudorache/Playground/tup-tup/tests/provisioner/workspace-bdc620cc-a70a-4dcc-8884-bd597200e45e',
+  //   artifactsPath:
+  //     '/Users/dragostudorache/Playground/tup-tup/tests/provisioner/workspace-bdc620cc-a70a-4dcc-8884-bd597200e45e/artifacts',
+  //   logsPath:
+  //     '/Users/dragostudorache/Playground/tup-tup/tests/provisioner/workspace-bdc620cc-a70a-4dcc-8884-bd597200e45e/logs',
+  //   appPath:
+  //     '/Users/dragostudorache/Playground/tup-tup/tests/provisioner/workspace-bdc620cc-a70a-4dcc-8884-bd597200e45e/app',
+  //   definition: {
+  //     name: 'my-pipeline',
+  //     jobs: [
+  //       // [Object ...], [Object ...]
+  //     ],
+  //   },
+  // }
   appPath: string
 }
