9b4206407fd9b4ce0238e6ada7c75d6e001730ec
Dragos Tudorache <hedphug@gmail.com>
2026-03-15 15:15:58 +0200

    Create workspace-describe.ts

---
+++ src/workspace-describe.ts
@@ -0,0 +1,40 @@
+import { afterAll, afterEach, beforeEach, describe } from 'bun:test'
+import {
+  filterRunningContainers,
+  removeContainerByName,
+} from '../tests/test-helpers/executor.test-helpers'
+import {
+  setupWorkspaceIn,
+  teardownWorkspaceIn,
+} from '../tests/test-helpers/workspace.test-helpers'
+
+export interface WorkspaceContext {
+  workspacePath: string
+}
+
+export function describeWithWorkspace(
+  name: string,
+  workingDir: string,
+  fn: (ctx: WorkspaceContext) => void,
+) {
+  describe(name, () => {
+    const ctx: WorkspaceContext = { workspacePath: '' }
+
+    beforeEach(() => {
+      ctx.workspacePath = setupWorkspaceIn(workingDir)
+    })
+
+    afterEach(() => {
+      teardownWorkspaceIn(ctx.workspacePath)
+    })
+
+    afterAll(async () => {
+      const runningContainers = await filterRunningContainers('tuptup')
+      for (const container of runningContainers) {
+        await removeContainerByName(container)
+      }
+    })
+
+    fn(ctx)
+  })
+}
