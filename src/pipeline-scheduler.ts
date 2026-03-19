240db7220f04c28745ddaaef05b608ac2b25a7ac
Dragos Tudorache <hedphug@gmail.com>
2026-03-19 23:19:47 +0200

    Refactor pipeline-scheduler

--- src/pipeline-scheduler.ts
+++ src/pipeline-scheduler.ts
@@ -1,108 +1,88 @@
 import type { ExecutorFactory } from './executor'
 import { Job } from './job'
-import type { JobReporter } from './job-reporter'
 import type { JobDefinition } from './job.types'
 import type { LoggerFactory } from './logger'
 import type { RuntimeContext } from './runtime-context'
 
+type JobNameKey = string
+type SettledJobResult = [boolean, JobDefinition, Error?]
+
+export type PipelineSchedulerEvent =
+  | { type: 'started'; name: string }
+  | { type: 'settled'; name: string; success: boolean; error?: Error }
+
 export class PipelineScheduler {
   private runtimeCtx: RuntimeContext
-  private jobReporter: JobReporter
   private fileLoggerFactory: LoggerFactory
   private dockerExecutorFactory: ExecutorFactory
 
-  // TODO we also need to store if the job succeeded or not, or has been skipped
-  // something like this: Map<string, { settled: boolean, success: boolean, jobDefinition: JobDefinition }>
-  //
-  // TODO don't think we need to keep the job definition here,
-  // b/c we already have the Map's keys as the job names, which we can use
-  // to get the job definition from the runtime context.
-  //
-  // "settled" means finished one way or another, either success, failure, or skipped
-  private settledJobs: Map<string, [boolean, JobDefinition]> = new Map()
-  private runningJobs: Map<string, Promise<[boolean, JobDefinition]>> =
-    new Map()
+  // "settled" means finished, either succeeded or failed
+  private settledJobs: Map<JobNameKey, SettledJobResult> = new Map()
+  private runningJobs: Map<JobNameKey, Promise<SettledJobResult>> = new Map()
 
   constructor(opts: {
     runtimeCtx: RuntimeContext
-    jobReporter: JobReporter
     fileLoggerFactory: LoggerFactory
     dockerExecutorFactory: ExecutorFactory
   }) {
     this.runtimeCtx = opts.runtimeCtx
-    this.jobReporter = opts.jobReporter
     this.fileLoggerFactory = opts.fileLoggerFactory
     this.dockerExecutorFactory = opts.dockerExecutorFactory
   }
 
-  async schedule() {
+  async *schedule(): AsyncGenerator<PipelineSchedulerEvent> {
     while (true) {
       const readyJobs = this.getReadyJobs()
-      console.log('readyJobs:', readyJobs.length)
       if (readyJobs.length === 0 && this.runningJobs.size === 0) break
 
-      console.log('running jobs:', Array.from(this.runningJobs.keys()))
-
-      const alreadyRunningJobs = Array.from(this.runningJobs.values())
-
+      // Build the next set of running jobs
       const nextRunningJobs = new Map(
         readyJobs.map(job => [job.name, this.runJob(job)]),
       )
 
-      nextRunningJobs.forEach((promise, jobName) => {
+      for (const [jobName, promise] of nextRunningJobs) {
         this.runningJobs.set(jobName, promise)
-      })
-      console.log('next running jobs:', Array.from(this.runningJobs.keys()))
+        yield { type: 'started', name: jobName }
+      }
 
-      // We need to wait for both the currently running jobs and the read ones
-      // if not, we'll simply skip the ones that might have not settled yet(and still running)
-      const jobResult = await Promise.race([
-        ...alreadyRunningJobs,
+      // Have to wait for both the running jobs and the newly/next added ones
+      // otherwise it simply skips the jobs that might have not settled yet(and still running)
+      const [success, jobDefinition, error] = await Promise.race([
+        ...this.runningJobs.values(),
         ...nextRunningJobs.values(),
       ])
-      console.log('job finished:', jobResult[1].name, 'success:', jobResult[0])
+      const { name } = jobDefinition
 
-      this.runningJobs.delete(jobResult[1].name)
-      console.log(
-        'remaining total jobs running:',
-        this.runtimeCtx.pipeline.jobs.length - this.settledJobs.size,
-      )
-      this.settledJobs.set(jobResult[1].name, jobResult)
+      this.runningJobs.delete(name)
+      this.settledJobs.set(name, [success, jobDefinition, error])
 
-      console.log('-------------------------------')
+      yield { type: 'settled', name, success, error }
     }
   }
 
-  private async runJob(jobDefinition: JobDefinition) {
+  private async runJob(
+    jobDefinition: JobDefinition,
+  ): Promise<[boolean, JobDefinition, Error?]> {
     const executor = this.dockerExecutorFactory.create(jobDefinition)
-
     const logger = this.fileLoggerFactory.create(jobDefinition.name)
 
-    let jobResult = [false, jobDefinition] as [boolean, JobDefinition]
+    let jobResult: [boolean, JobDefinition, Error?]
 
     try {
       await executor.start()
-
-      const job = new Job({
+      jobResult = await new Job({
         definition: jobDefinition,
-        reporter: this.jobReporter,
         logger,
         executor,
-      })
-
-      const runResult = await job.run()
-      // jobResult = [runResult, jobDefinition]
-      jobResult = runResult
+      }).run()
     } catch (err) {
-      // TODO need more than this
-      // Maybe we should just re-throw the error and let the caller handle it,
-      // for example, if the executor fails to start, what do we do?
-      // Do we mark the job as failed and move on to the next one? Do we retry? Do we stop the whole pipeline?
-      //
-      // Actually, this could fail in the middle of the pipeline execution
-      console.error(`Error running job ${jobDefinition.name}:`, err)
-
-      jobResult = [false, jobDefinition]
+      jobResult = [
+        false,
+        jobDefinition,
+        err instanceof Error
+          ? err
+          : new Error('Unknown pipeline schedule error'),
+      ]
     } finally {
       await executor.stop()
       await logger.stop()
@@ -122,6 +102,7 @@ export class PipelineScheduler {
       if (!job.dependsOn) return true
 
       return job.dependsOn.every(
+        // Is the dependency settled and succeeded?
         dep => this.settledJobs.get(dep)?.at(0) === true,
       )
     })
--- tests/pipeline-scheduler/pipeline-scheduler.test.ts
+++ tests/pipeline-scheduler/pipeline-scheduler.test.ts
@@ -1,8 +1,9 @@
 import { expect, it } from 'bun:test'
+import { name } from 'node:assert'
+import { type } from 'node:os'
 import { DockerExecutor } from '../../src/docker-executor'
 import { PipelineScheduler } from '../../src/pipeline-scheduler'
 import type { PipelineDefinition } from '../../src/pipeline.types'
-import { TestEventsReporter } from '../__helpers__/test-events-reporter'
 import { TestListLogger } from '../__helpers__/test-list-logger'
 import {
   describeWithWorkspace,
@@ -11,13 +12,18 @@ import {
 
 describeWithWorkspace('Pipline Scheduler', './tests/runner', ctx => {
   it('should handle a simple parallel job pipeline', async () => {
-    const { coordinator, reporter } = setupCoordinator(ctx, {
+    console.time('------------')
+
+    const coordinator = setupCoordinator(ctx, {
       name: 'my-pipeline',
       jobs: [
         {
           name: 'job_a',
           image: 'busybox',
-          commands: [['echo', 'Hello from job_a!']],
+          commands: [
+            ['sleep', '1'],
+            ['echo', 'Hello from job_a!'],
+          ],
         },
         {
           name: 'job_b',
@@ -27,16 +33,39 @@ describeWithWorkspace('Pipline Scheduler', './tests/runner', ctx => {
       ],
     })
 
-    await coordinator.run()
+    const events = []
 
-    // console.log('ctx:', ctx)
-    // console.log('reporter:', reporter.events)
+    for await (const event of coordinator.schedule()) {
+      // console.log('event:', event)
+      events.push(event)
+    }
 
-    expect(reporter.events.length).toBe(8)
+    expect(events).toMatchObject([
+      {
+        type: 'started',
+        name: 'job_a',
+      },
+      {
+        type: 'started',
+        name: 'job_b',
+      },
+      {
+        type: 'settled',
+        name: 'job_b',
+        success: true,
+        error: undefined,
+      },
+      {
+        type: 'settled',
+        name: 'job_a',
+        success: true,
+        error: undefined,
+      },
+    ])
   })
 
   it('should handle a simple dependant job pipeline', async () => {
-    const { coordinator, reporter } = setupCoordinator(ctx, {
+    const coordinator = setupCoordinator(ctx, {
       name: 'my-pipeline',
       jobs: [
         {
@@ -66,34 +95,152 @@ describeWithWorkspace('Pipline Scheduler', './tests/runner', ctx => {
       ],
     })
 
-    await coordinator.run()
+    const events = []
 
-    const events = reporter.events
+    for await (const event of coordinator.schedule()) {
+      // console.log('event:', event)
+      events.push(event)
+    }
 
-    expect(events.length).toBe(16)
-    expect(events[0]).toMatchObject({ jobName: 'job_c', type: 'job:started' })
-    expect(events[4]).toMatchObject({ jobName: 'job_a', type: 'job:started' })
-    expect(events.findIndex(e => e.jobName === 'job_b')).toBeGreaterThan(
-      events.findIndex(e => e.jobName === 'job_a'),
-    )
-    expect(events.findIndex(e => e.jobName === 'job_d')).toBeGreaterThan(
-      events.findIndex(e => e.jobName === 'job_a'),
-    )
-    expect(events.findIndex(e => e.jobName === 'job_d')).toBeGreaterThan(
-      events.findIndex(e => e.jobName === 'job_c'),
-    )
+    const allButLastTwo = events.slice(0, -2)
+    const lastTwo = events.slice(-2)
+
+    // Strict order for deterministic part
+    expect(allButLastTwo).toEqual([
+      {
+        type: 'started',
+        name: 'job_c',
+      },
+      {
+        type: 'settled',
+        name: 'job_c',
+        success: true,
+        error: undefined,
+      },
+      {
+        type: 'started',
+        name: 'job_a',
+      },
+      {
+        type: 'settled',
+        name: 'job_a',
+        success: true,
+        error: undefined,
+      },
+      {
+        type: 'started',
+        name: 'job_b',
+      },
+      {
+        type: 'started',
+        name: 'job_d',
+      },
+    ])
+
+    // Last two jobs can be in any order since they run in parallel
+    expect(lastTwo).toHaveLength(2)
+    expect(lastTwo.map(e => e.name).sort()).toEqual(['job_b', 'job_d'])
+  })
+
+  it('should skip following jobs after a dependant failed', async () => {
+    const coordinator = setupCoordinator(ctx, {
+      name: 'my-pipeline',
+      jobs: [
+        {
+          name: 'job_a',
+          image: 'busybox',
+          commands: [['sh', '-c', 'exit 1']],
+        },
+        {
+          name: 'job_b',
+          image: 'busybox',
+          commands: [['echo', 'Hello from job_b!']],
+          dependsOn: ['job_a'],
+        },
+      ],
+    })
+
+    const events = []
+
+    for await (const event of coordinator.schedule()) {
+      // console.log('event:', event)
+      events.push(event)
+    }
+
+    expect(events).toMatchObject([
+      {
+        type: 'started',
+        name: 'job_a',
+      },
+      {
+        type: 'settled',
+        name: 'job_a',
+        success: false,
+        error: undefined,
+      },
+    ])
+  })
+
+  it.only('should see errors???', async () => {
+    const coordinator = setupCoordinator(ctx, {
+      name: 'my-pipeline',
+      jobs: [
+        {
+          name: 'job_a',
+          image: 'busybox',
+          commands: [['echo', 'hello world']],
+        },
+        {
+          name: 'job_b',
+          image: 'inexistent-image',
+          commands: [['echo', 'Hello from job_b!']],
+          dependsOn: ['job_a'],
+        },
+      ],
+    })
+
+    const events = []
+
+    for await (const event of coordinator.schedule()) {
+      // console.log('event:', event)
+      events.push(event)
+    }
+
+    expect(events).toMatchObject([
+      {
+        type: 'started',
+        name: 'job_a',
+      },
+      {
+        type: 'settled',
+        name: 'job_a',
+        success: true,
+        error: undefined,
+      },
+      {
+        type: 'started',
+        name: 'job_b',
+      },
+      {
+        type: 'settled',
+        name: 'job_b',
+        success: false,
+        error: expect.objectContaining({
+          message: expect.stringContaining(
+            "Unable to find image 'inexistent-image:latest' locally",
+          ),
+        }),
+      },
+    ])
   })
 })
 
 function setupCoordinator(ctx: WorkspaceContext, pipeline: PipelineDefinition) {
-  const reporter = new TestEventsReporter()
-
   const coordinator = new PipelineScheduler({
     runtimeCtx: {
       ...ctx,
       pipeline,
     },
-    jobReporter: reporter,
     fileLoggerFactory: { create: () => new TestListLogger() },
     dockerExecutorFactory: {
       create: (opts: { image: string; name: string }) => {
@@ -106,8 +253,5 @@ function setupCoordinator(ctx: WorkspaceContext, pipeline: PipelineDefinition) {
     },
   })
 
-  return {
-    coordinator,
-    reporter,
-  }
+  return coordinator
 }
